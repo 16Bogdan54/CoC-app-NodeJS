67181fc8b04251dc224c9e91fe93b7db52669913
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-03 00:00:18 +0200

    devltools

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import PlayerProfile from "./components/client/PlayerProfile";
 
 function App() {
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 1000 * 60 * 5 } },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
