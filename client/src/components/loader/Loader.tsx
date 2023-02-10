f44cb3afdd019c19dbc7ec8f290ca26b07405b0e
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-10 17:14:47 +0200

    loader

--- client/src/components/loader/Loader.tsx
+++ client/src/components/loader/Loader.tsx
@@ -1,7 +1,11 @@
 import { ClipLoader } from "react-spinners";
 
 const Loader = () => {
-  return <ClipLoader color="#36d7b7" />;
+  return (
+    <div className="h-[90vh] w-full flex justify-center items-center">
+      <ClipLoader color="#36d7b7" />
+    </div>
+  );
 };
 
 export default Loader;
