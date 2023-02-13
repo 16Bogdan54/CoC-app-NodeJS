c38cd446d59e24cf6e0672e1ed81f7f75010a665
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-13 23:58:38 +0200

    clan capital

--- client/src/pages/myClan/clanCapital/MyClanCapital.tsx
+++ client/src/pages/myClan/clanCapital/MyClanCapital.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ClanDescription from "@/components/clanDescription/ClanDescription";
 import ClanCapitalStat from "@/components/clanCapitalStat/ClanCapitalStat";
+import MembersHomeVillage from "@/components/membersHomeVillage/MembersHomeVillage";
 
 const MyClanCapital = () => {
   return (
     <div>
       <ClanDescription />
       <ClanCapitalStat />
+      <MembersHomeVillage />
     </div>
   );
 };
