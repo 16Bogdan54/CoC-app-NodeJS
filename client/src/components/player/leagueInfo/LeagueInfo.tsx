a1ce6b679947ffb1db86af92db0dc58af79a2f4f
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-14 22:16:42 +0200

    league info styles

--- client/src/components/player/leagueInfo/LeagueInfo.tsx
+++ client/src/components/player/leagueInfo/LeagueInfo.tsx
@@ -1,23 +1,31 @@
 import { useQueryCache } from "@/hooks/useQueryCache";
 import { Player } from "clashofclans.js";
 
+import style from "./league.module.css";
+
 const LeagueInfo = () => {
   const player = useQueryCache<Player>("playerData");
 
   return (
     <div>
-      <div>
+      <div className={style.league_block}>
         <img width={100} src={player.league.icon.url} alt="league icon" />
-        <h1>{player.league.name}</h1>
-        <h1>{player.trophies}</h1>
+        <div>
+          <h1>{player.league.name}</h1>
+          <h1>{player.trophies}</h1>
+        </div>
       </div>
-      <div>
-        <h1>War Stars Won:</h1>
-        <h1>{player.warStars}</h1>
-      </div>
-      <div>
-        <h1>All time best:</h1>
-        <h1>{player.bestTrophies}</h1>
+      <div className="flex">
+        <div className={style.war_stars_block}>
+          <h1>War Stars Won:</h1>
+          <h1>{player.warStars}</h1>
+        </div>
+        <div className={style.trophies_block}>
+          <h1>All time best:</h1>
+          <div className="flex">
+            <h1>{player.bestTrophies}</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
