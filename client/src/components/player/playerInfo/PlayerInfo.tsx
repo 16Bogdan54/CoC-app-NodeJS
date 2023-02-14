c1b3755d17f302be1ab41cba48905e0f780def9d
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-14 21:49:22 +0200

    player info

--- client/src/components/player/playerInfo/PlayerInfo.tsx
+++ client/src/components/player/playerInfo/PlayerInfo.tsx
@@ -1,20 +1,25 @@
 import { useQueryCache } from "@/hooks/useQueryCache";
 import { Player } from "clashofclans.js";
-import ExperienceLevel from "@/components/player/playerInfo/expirienceLevel/ExperienceLevel";
+import style from "./player_info.module.css";
 
 const PlayerInfo = () => {
   const player = useQueryCache<Player>("playerData");
 
   return (
-    <div>
-      {/*<h1>{player.expLevel}*/}
-      <ExperienceLevel expLvl={player.expLevel} />
-      <h1>{player.name}</h1>
-      <h1>{player.tag}</h1>
-      <h1>{player.role}</h1>
-      {player.labels.map((label, index) => (
-        <img width={30} key={index} src={label.icon.url} alt="label" />
-      ))}
+    <div className="w-fit">
+      <div className="flex items-center">
+        <div className={style.exp_lvl}>{player.expLevel}</div>
+        <div>
+          <h2 className="text-2xl text-white ">{player.name}</h2>
+          <span className="text-lg text-gray-200">{player.tag}</span>
+          <h3 className="text-xl text-white">{player.role}</h3>
+        </div>
+      </div>
+      <div className={style.labels_block}>
+        {player.labels.map((label, index) => (
+          <img width={30} key={index} src={label.icon.url} alt="label" />
+        ))}
+      </div>
     </div>
   );
 };
