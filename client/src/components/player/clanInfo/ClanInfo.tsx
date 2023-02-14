8cb9c18c12638c0549a4f7e13d89f689c5e3c8d2
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-14 22:01:08 +0200

    clan info

--- client/src/components/player/clanInfo/ClanInfo.tsx
+++ client/src/components/player/clanInfo/ClanInfo.tsx
@@ -1,12 +1,14 @@
 import { useQueryCache } from "@/hooks/useQueryCache";
 import { Player } from "clashofclans.js";
 
+import style from "./clanInfo.module.css";
+
 const ClanInfo = () => {
   const player = useQueryCache<Player>("playerData");
 
   return (
-    <div>
-      <h1>{player.clan?.name}</h1>
+    <div className={style.clanInfo_container}>
+      <h2 className="text-2xl">{player.clan?.name}</h2>
       <img width={100} src={player.clan?.badge.url} alt="clan logo" />
     </div>
   );
