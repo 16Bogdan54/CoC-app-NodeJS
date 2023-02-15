bc60a6d579c4ea2922c0a7481e1558bba8206dba
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-15 15:00:33 +0200

    ÐгтÑunits layout

--- client/src/components/troops/Units.tsx
+++ client/src/components/troops/Units.tsx
@@ -3,13 +3,21 @@ import HeroesList from "@/components/troops/heroesList/HeroesList";
 import SpellList from "@/components/troops/spellList/SpellList";
 import SiegeMachineList from "@/components/troops/siegeMachineList/SiegeMachineList";
 
+import style from "./styles/units.module.css";
+
 const Units = () => {
   return (
-    <div>
-      <TroopsList />
-      <HeroesList />
-      <SpellList />
-      <SiegeMachineList />
+    <div className={style.units_container}>
+      <div className={style.units_inner}>
+        <div>
+          <HeroesList />
+        </div>
+        <div>
+          <TroopsList />
+          <SpellList />
+          <SiegeMachineList />
+        </div>
+      </div>
     </div>
   );
 };
