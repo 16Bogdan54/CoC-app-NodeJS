5e495d9dac169e02db276c6239f846bfc37b043b
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-15 17:12:38 +0200

    spells

--- client/src/components/troops/spellList/SpellList.tsx
+++ client/src/components/troops/spellList/SpellList.tsx
@@ -1,17 +1,15 @@
 import { getIconURL } from "@/components/utils/utils";
 import { useQueryCache } from "@/hooks/useQueryCache";
 import { Player } from "clashofclans.js";
+import Unit from "@/components/troops/unit/Unit";
 
 const SpellList = () => {
   const player = useQueryCache<Player>("playerData");
 
   return (
-    <div className="w-full flex items-center justify-center">
+    <div className="flex items-center justify-start flex-wrap gap-2 my-2">
       {player.spells.map((spell, index) => (
-        <div key={index}>
-          <img width={50} src={getIconURL(spell.name)} alt="icon" />
-          <h1>{spell.level}</h1>
-        </div>
+        <Unit key={index} picURL={getIconURL(spell.name)} lvl={spell.level} />
       ))}
     </div>
   );
