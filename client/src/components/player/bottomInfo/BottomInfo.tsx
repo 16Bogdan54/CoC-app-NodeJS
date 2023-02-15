3f64a5db7878daf523f49197d9ae71eb490e2e19
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-15 14:15:45 +0200

    bottom panel

--- client/src/components/player/bottomInfo/BottomInfo.tsx
+++ client/src/components/player/bottomInfo/BottomInfo.tsx
@@ -1,18 +1,20 @@
 import { useQueryCache } from "@/hooks/useQueryCache";
 import { Player } from "clashofclans.js";
 
+import style from "./bottom.module.css";
+
 const BottomInfo = () => {
   const player = useQueryCache<Player>("playerData");
 
   return (
-    <div className="flex justify-between align-center bg-[#494c79] text-white">
+    <div className={style.bottom_panel}>
       <div>
         <span>Troops donated:</span>
         <span>{player.donations}</span>
       </div>
       <div>
         <span>Troops received:</span>
-        <span>{player.received}:</span>
+        <span>{player.received}</span>
       </div>
       <div>
         <span>Attacks Won:</span>
