514b609775a730d5f1f49b870a76b4eb55b55e4e
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-15 17:22:15 +0200

    siege machinese style

--- client/src/components/troops/siegeMachineList/SiegeMachineList.tsx
+++ client/src/components/troops/siegeMachineList/SiegeMachineList.tsx
@@ -1,19 +1,25 @@
 import { useQueryCache } from "@/hooks/useQueryCache";
 import { getIconURL } from "@/components/utils/utils";
 import { Player } from "clashofclans.js";
+import Unit from "@/components/troops/unit/Unit";
 
 const SiegeMachineList = () => {
   const player = useQueryCache<Player>("playerData");
 
   return (
-    <div className="w-full flex items-center justify-center">
+    <div className="flex items-center justify-start flex-wrap gap-2 my-2">
       {player.troops.map((troop, index) => {
         if (troop.unlockBuilding === "Workshop") {
           return (
-            <div key={index}>
-              <img width={50} src={getIconURL(troop.name)} alt="icon" />
-              <h1>{troop.level}</h1>
-            </div>
+            <Unit
+              key={index}
+              picURL={getIconURL(troop.name)}
+              lvl={troop.level}
+            />
+            // <div key={index}>
+            //   <img width={50} src={getIconURL(troop.name)} alt="icon" />
+            //   <h1>{troop.level}</h1>
+            // </div>
           );
         }
       })}
