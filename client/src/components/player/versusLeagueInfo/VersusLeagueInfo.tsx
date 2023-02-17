3bc6912a02780ed46a58a43604a54ae70715f9ed
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-17 12:50:50 +0200

    trophies style

--- client/src/components/player/versusLeagueInfo/VersusLeagueInfo.tsx
+++ client/src/components/player/versusLeagueInfo/VersusLeagueInfo.tsx
@@ -1,13 +1,20 @@
 import { useQueryCache } from "@/hooks/useQueryCache";
 import { Player } from "clashofclans.js";
 
+import style from "./league.module.css";
+
 const VersusLeagueInfo = () => {
   const player = useQueryCache<Player>("playerData");
 
   return (
-    <div>
-      <h1>{player.versusTrophies}</h1>
-      <h1>{player.bestVersusTrophies}</h1>
+    <div className={style.league_container}>
+      <div>
+        <span>🏆{player.versusTrophies}</span>
+      </div>
+      <div>
+        <h2>All time best:</h2>
+        <span>🏆{player.bestVersusTrophies}</span>
+      </div>
     </div>
   );
 };
