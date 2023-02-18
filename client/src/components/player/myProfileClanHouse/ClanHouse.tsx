f3c10aad17f1c88fdca3b467a8c20cebba76abf8
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-18 20:05:35 +0200

    clan capital style

--- client/src/components/player/myProfileClanHouse/ClanHouse.tsx
+++ client/src/components/player/myProfileClanHouse/ClanHouse.tsx
@@ -1,13 +1,13 @@
-import React from "react";
 import { useQueryCache } from "@/hooks/useQueryCache";
 import { Player } from "clashofclans.js";
+
+import style from "./clanhouse.module.css";
 
 const ClanHouse = () => {
   const player = useQueryCache<Player>("playerData");
 
   return (
-    <div>
-      <h1>Clan House</h1>
+    <div className={style.capital_container}>
       <h1>total Capital Contribution</h1>
       <span>{player.clanCapitalContributions}</span>
     </div>
