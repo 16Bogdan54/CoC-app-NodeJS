890e7b7a5fec5aee50fd0558281075e2ad5caf9b
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-19 10:29:52 +0200

    bulider base member style

--- client/src/components/membersBuilderBase/MembersBuilderBase.tsx
+++ client/src/components/membersBuilderBase/MembersBuilderBase.tsx
@@ -7,12 +7,17 @@ const MembersBuilderBase = () => {
   return (
     <div>
       {clan.members.map((member, index) => (
-        <div key={index}>
-          <h1>{member.clanRank}</h1>
+        <div
+          className="flex items-center justify-between gap-6 p-3 bg-[#d5d8cc] m-2 rounded-lg"
+          key={index}
+        >
+          <h1>{member.clanRank}.</h1>
           <h1>{member.expLevel}</h1>
-          <h1>{member.name}</h1>
-          <h1>{member.role}</h1>
-          <h1>{member.versusTrophies}</h1>
+          <div className="flex-1">
+            <h1>{member.name}</h1>
+            <h1>{member.role}</h1>
+          </div>
+          <span className="">{member.versusTrophies}🏆</span>
         </div>
       ))}
     </div>
