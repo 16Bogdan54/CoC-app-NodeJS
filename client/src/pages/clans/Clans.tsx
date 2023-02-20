7828567192d9ba175c8f5421a8dbf44ef61e5bf6
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-20 13:06:24 +0200

    forms

--- client/src/pages/clans/Clans.tsx
+++ client/src/pages/clans/Clans.tsx
@@ -1,13 +1,18 @@
 import { motion } from "framer-motion";
+import { Button, TextField } from "@mui/material";
 
 const Clans = () => {
   return (
     <motion.div
+      className="bg-[#e9e9e1] p-3 min-h-[95vh]"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <h1>Clans</h1>
+      <TextField id="outlined-basic" label="Clan Tag" variant="outlined" />
+      <Button variant="contained" size="large">
+        Search
+      </Button>
     </motion.div>
   );
 };
--- client/src/pages/profiles/Profiles.tsx
+++ client/src/pages/profiles/Profiles.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import { Button, TextField } from "@mui/material";
+import { motion } from "framer-motion";
 
 const Profiles = () => {
   return (
-    <div>
-      <h1>Profiles</h1>
-    </div>
+    <motion.div
+      className="bg-[#e9e9e1] p-3 min-h-[95vh]"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <TextField id="outlined-basic" label="Player Tag" variant="outlined" />
+      <Button variant="contained" size="large">
+        Search
+      </Button>
+    </motion.div>
   );
 };
 
