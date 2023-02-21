11bcdb12523ee06f093ddfcf46b4e5aa9e331838
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-21 15:06:56 +0200

    deleted comment

--- client/src/hooks/useQueryCache.ts
+++ client/src/hooks/useQueryCache.ts
@@ -1,5 +1,4 @@
 import { useQueryClient } from "@tanstack/react-query";
-// import { Player } from "clashofclans.js";
 
 export const useQueryCache = <T>(key: string): T => {
   const client = useQueryClient();
