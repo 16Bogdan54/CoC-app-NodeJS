2d05cdd51ac3e857c51d3bf03f3e9b8eed66b3e0
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-28 21:47:07 +0200

    mutations

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,9 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
-const queryClient = new QueryClient({});
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 100 } },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
