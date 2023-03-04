6618852fcc8cc6b84077f74afc130d13f4ebafd6
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-04 20:59:12 +0200

    navbar

--- client/src/components/navbar/myProfileSubNavbar/MyProfileSubNavbar.tsx
+++ client/src/components/navbar/myProfileSubNavbar/MyProfileSubNavbar.tsx
@@ -1,12 +1,22 @@
 import NavButton from "@/components/navbar/navButton/NavButton";
+import { Stack } from "@mui/material";
 
 const MyProfileSubNavbar = () => {
   return (
-    <div className="flex justify-evenly">
+    <Stack
+      direction="column"
+      sx={{ position: "fixed" }}
+      spacing={{ xs: 1, sm: 2, md: 4 }}
+    >
       <NavButton to="/">Home Village</NavButton>
       <NavButton to="/my-profile/builder-base-profile">Builder Base</NavButton>
       <NavButton to="/my-profile/clan-capital-profile">Clan Capital</NavButton>
-    </div>
+    </Stack>
+    // <div className="flex justify-evenly">
+    //   <NavButton to="/">Home Village</NavButton>
+    //   <NavButton to="/my-profile/builder-base-profile">Builder Base</NavButton>
+    //   <NavButton to="/my-profile/clan-capital-profile">Clan Capital</NavButton>
+    // </div>
   );
 };
 
