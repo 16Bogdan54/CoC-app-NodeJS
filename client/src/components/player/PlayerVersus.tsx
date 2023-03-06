583ebea961f35a2072ba82ee9b6864841a95da84
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-06 19:00:29 +0200

    player builder base

--- client/src/components/player/PlayerVersus.tsx
+++ client/src/components/player/PlayerVersus.tsx
@@ -1,6 +1,5 @@
 import PlayerInfo from "@/components/player/playerInfo/PlayerInfo";
 import ClanInfo from "@/components/player/clanInfo/ClanInfo";
-import VersusLeagueInfo from "@/components/player/versusLeagueInfo/VersusLeagueInfo";
 import VersusBottomStat from "@/components/player/versusBottomStat/VersusBottomStat";
 import { Grid } from "@mui/material";
 
@@ -13,9 +12,6 @@ const PlayerVersus = () => {
       <Grid item xs={6}>
         <ClanInfo />
       </Grid>
-      {/*<Grid item xs={6} md={4}>*/}
-      {/*  <VersusLeagueInfo />*/}
-      {/*</Grid>*/}
       <Grid item xs={12}>
         <VersusBottomStat />
       </Grid>
--- client/src/components/player/versusBottomStat/VersusBottomStat.tsx
+++ client/src/components/player/versusBottomStat/VersusBottomStat.tsx
@@ -10,30 +10,19 @@ const VersusBottomStat = () => {
   return (
     <Grid container className={style.bottom_panel} p={2}>
       <Grid item xs={6} sm={4}>
-        <Typography variant="body2">Versus {player.versusTrophies}</Typography>
+        <Typography variant="body1">Versus {player.versusTrophies}</Typography>
       </Grid>
       <Grid item xs={6} sm={4}>
-        <Typography variant="body2">
+        <Typography variant="body1">
           All time best: {player.bestVersusTrophies}
         </Typography>
       </Grid>
-      <Grid item xs={6} sm={4}>
-        <Typography variant="body2">
+      <Grid item xs={12} sm={4}>
+        <Typography variant="body1">
           Versus Battle Wins: {player.versusBattleWins}
         </Typography>
       </Grid>
     </Grid>
-    //   {/*<div className={style.bottom_panel}>*/}
-    //       // <div>
-    //       //     <span>🏆{player.versusTrophies}</span>
-    //       // </div>
-    //       // <div>
-    //       //     <h2>All time best:</h2>
-    //       //     <span>🏆{player.bestVersusTrophies}</span>
-    //       // </div>
-    //     // <span className="mr-2">Versus Battle Wins:</span>
-    //     // <span>{player.versusBattleWins}</span>
-    //   {/*</div>*/}
   );
 };
 
