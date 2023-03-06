dcdc9a5c8f8763be3ce97f49ba0a74f5565f570e
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-06 17:51:24 +0200

    siege machine style

--- client/src/components/troops/siegeMachineList/SiegeMachineList.tsx
+++ client/src/components/troops/siegeMachineList/SiegeMachineList.tsx
@@ -1,17 +1,16 @@
 import { useQueryCache } from "@/hooks/useQueryCache";
 import { getIconURL } from "@/components/utils/utils";
 import { Player } from "clashofclans.js";
+import { Box, Stack, Typography } from "@mui/material";
 import Unit from "@/components/troops/unit/Unit";
-
-import style from "@/components/troops/styles/units.module.css";
 
 const SiegeMachineList = () => {
   const player = useQueryCache<Player>("playerData");
 
   return (
-    <div className={style.units_outer}>
-      <h2 className={style.units_heading}>Siege Machines</h2>
-      <div className={style.img_container}>
+    <Box className="text-white" p={2}>
+      <Typography variant="h5">Siege Machines</Typography>
+      <Stack direction="row" spacing={2} py={1}>
         {player.troops.map((troop, index) => {
           if (troop.unlockBuilding === "Workshop") {
             return (
@@ -23,8 +22,8 @@ const SiegeMachineList = () => {
             );
           }
         })}
-      </div>
-    </div>
+      </Stack>
+    </Box>
   );
 };
 
