368f8c21e858a2fff0d5b6137e323beb5aca7837
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-06 17:57:02 +0200

    troops

--- client/src/components/troops/troopsList/TroopsList.tsx
+++ client/src/components/troops/troopsList/TroopsList.tsx
@@ -1,17 +1,16 @@
 import Unit from "@/components/troops/unit/Unit";
 import { getIconURL } from "@/components/utils/utils";
 import { useQueryCache } from "@/hooks/useQueryCache";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 import { Player } from "clashofclans.js";
-
-import style from "@/components/troops/styles/units.module.css";
 
 const TroopsList = () => {
   const player = useQueryCache<Player>("playerData");
 
   return (
-    <div className={style.units_outer}>
-      <h2 className={style.units_heading}>Troops</h2>
-      <div className={style.img_container}>
+    <Box className="text-white" p={2}>
+      <Typography variant="h5">Troops</Typography>
+      <Grid container spacing={2} py={1}>
         {player.troops.map((troop, index) => {
           if (
             troop.village === "home" &&
@@ -19,16 +18,18 @@ const TroopsList = () => {
             troop.unlockBuilding !== "Workshop"
           ) {
             return (
-              <Unit
-                key={index}
-                picURL={getIconURL(troop.name)}
-                lvl={troop.level}
-              />
+              <Grid item>
+                <Unit
+                  key={index}
+                  picURL={getIconURL(troop.name)}
+                  lvl={troop.level}
+                />
+              </Grid>
             );
           }
         })}
-      </div>
-    </div>
+      </Grid>
+    </Box>
   );
 };
 
