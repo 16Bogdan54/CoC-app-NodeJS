f5298f4c57f630d3f229f71f78717123878e694c
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-06 17:34:50 +0200

    player info

--- client/src/components/player/bottomInfo/BottomInfo.tsx
+++ client/src/components/player/bottomInfo/BottomInfo.tsx
@@ -2,29 +2,34 @@ import { useQueryCache } from "@/hooks/useQueryCache";
 import { Player } from "clashofclans.js";
 
 import style from "./bottom.module.css";
+import { Grid, Typography } from "@mui/material";
 
 const BottomInfo = () => {
   const player = useQueryCache<Player>("playerData");
 
   return (
-    <div className={style.bottom_panel}>
-      <div>
-        <span>Troops donated:</span>
-        <span>{player.donations}</span>
-      </div>
-      <div>
-        <span>Troops received:</span>
-        <span>{player.received}</span>
-      </div>
-      <div>
-        <span>Attacks Won:</span>
-        <span>{player.attackWins}</span>
-      </div>
-      <div>
-        <span>Defenses Won:</span>
-        <span>{player.defenseWins}</span>
-      </div>
-    </div>
+    <Grid container className={style.bottom_panel} p={2}>
+      <Grid item xs={6} sm={3}>
+        <Typography variant="body2">
+          Troops donated: {player.donations}
+        </Typography>
+      </Grid>
+      <Grid item xs={6} sm={3}>
+        <Typography variant="body2">
+          Troops received: {player.received}
+        </Typography>
+      </Grid>
+      <Grid item xs={6} sm={3}>
+        <Typography variant="body2">
+          Attacks Won: {player.attackWins}
+        </Typography>
+      </Grid>
+      <Grid item xs={6} sm={3}>
+        <Typography variant="body2">
+          Defenses Won: {player.defenseWins}
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
 
