a7ede5902ba4ad5d7e8488cde6fcedc46b95267e
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-06 19:22:36 +0200

    capital city

--- client/src/components/player/myProfileClanHouse/ClanHouse.tsx
+++ client/src/components/player/myProfileClanHouse/ClanHouse.tsx
@@ -2,15 +2,17 @@ import { useQueryCache } from "@/hooks/useQueryCache";
 import { Player } from "clashofclans.js";
 
 import style from "./clanhouse.module.css";
+import { Box, Typography } from "@mui/material";
 
 const ClanHouse = () => {
   const player = useQueryCache<Player>("playerData");
 
   return (
-    <div className={style.capital_container}>
-      <h1>total Capital Contribution</h1>
-      <span>{player.clanCapitalContributions}</span>
-    </div>
+    <Box textAlign="center" className="bg-[#3681D8] rounded-md p-2">
+      <Typography variant="body1">
+        Total Capital Contribution: {player.clanCapitalContributions}
+      </Typography>
+    </Box>
   );
 };
 
